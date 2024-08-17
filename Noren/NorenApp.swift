2481a018c90138e058f1f5e8280f4870f09a513d
リョウ <121738322+RyoDeveloper@users.noreply.github.com>
2024-08-17 23:25:38 +0900

    Initial Commit

---
+++ Noren/NorenApp.swift
@@ -0,0 +1,18 @@
+//
+//  NorenApp.swift
+//  Noren
+//  
+//  https://github.com/RyoDeveloper/Noren
+//  Copyright © 2024 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct NorenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
