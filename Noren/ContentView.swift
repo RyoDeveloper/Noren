4a219bf4a12762d93125dc6270423c8c2bc17556
リョウ <121738322+RyoDeveloper@users.noreply.github.com>
2024-08-18 10:54:53 +0900

    Add TabView

--- Noren/ContentView.swift
+++ Noren/ContentView.swift
@@ -1,22 +1,42 @@
 //
 //  ContentView.swift
 //  Noren
-//  
+//
 //  https://github.com/RyoDeveloper/Noren
 //  Copyright © 2024 RyoDeveloper. All rights reserved.
 //
 
 import SwiftUI
 
+enum TabItem {
+    case hailight
+    case calendar
+    case reminder
+    case note
+    case search
+}
+
 struct ContentView: View {
+    @State var selectedTab: TabItem = .hailight
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $selectedTab) {
+            Tab("ハイライト", systemImage: "sparkles", value: .hailight) {
+                Text("ハイライト")
+            }
+            Tab("カレンダー", systemImage: "calendar", value: .calendar) {
+                Text("カレンダー")
+            }
+            Tab("リマインダー", systemImage: "checklist", value: .reminder) {
+                Text("リマインダー")
+            }
+            Tab("ノート", systemImage: "note.text", value: .note) {
+                Text("ノート")
+            }
+            Tab(value: .search, role: .search) {
+                Text("検索")
+            }
         }
-        .padding()
     }
 }
 
